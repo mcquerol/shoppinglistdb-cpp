aef92b56333c432c4c9c6c47ec73384f428dfade
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-05 17:55:40 +0200

    implemented print method

--- myCode/ShoppingList.cpp
+++ myCode/ShoppingList.cpp
@@ -16,7 +16,28 @@ ShoppingList& ShoppingList::operator +=(Item *item)
 // and print its content at the end.
 void ShoppingList::print(time_t until) const
 {
+	auto notesSet = set<string>();
+	int noteNum = 0;
 
+	cout << "Shopping List: " << endl;
+	for(const auto& item: items)
+	{
+		cout << item.first << ":" << endl;
+		for(const auto& vec : item.second)
+		{
+			cout << *vec.get() << endl;
+			notesSet = vec.get()->getNotes();
+		}
+		cout << endl;
+	}
+
+	if(!notesSet.empty())
+	{
+		for (const auto& note : notesSet)
+		{
+		    cout << "[Note " << noteNum++ << "] " << note << "\n";
+		}
+	}
 }
 
 // Task 7: Provide implementation of save (3 points)
